928fc8cbbc3b2e1dbbbcd9fa393d04335c5477a5
Dilum Harshana <dilum.harshana@itelasoft.com>
2025-06-02 16:41:55 +0530

    fix - fixed issues in reading env files

--- src/Configs/Api.ts
+++ src/Configs/Api.ts
@@ -12,7 +12,6 @@ export const API_ENDPOINTS = {
 } as const;
 
 export const API_CONFIG = {
-  BASE_URL:
-    import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api",
+  BASE_URL: import.meta.env.VITE_REACT_APP_API_BASE_URL,
   TIMEOUT: 10000
 } as const;
--- src/Utilis/ApiClient.ts
+++ src/Utilis/ApiClient.ts
@@ -10,6 +10,8 @@ class ApiClient {
   private client: AxiosInstance;
 
   constructor() {
+    console.log(API_CONFIG.BASE_URL);
+
     this.client = axios.create({
       baseURL: API_CONFIG.BASE_URL,
       timeout: API_CONFIG.TIMEOUT,
